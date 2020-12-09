0e97a2b35be9aa63f4dc02748f74c29fb25619a6
Africa Agwu <agwuafricaagwu@gmail.com>
2020-12-09 17:46:21 +0100

    Scaffolded Identity for customization

---
+++ Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using SharpCodeWebsite.Data;
+
+[assembly: HostingStartup(typeof(SharpCodeWebsite.Areas.Identity.IdentityHostingStartup))]
+namespace SharpCodeWebsite.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
