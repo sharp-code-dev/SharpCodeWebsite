7cd6405f42e719b8bf9ad1c560e3dac2f4db1158
meredaniprogrammer <mereugochukwudaniel@gmail.com>
2020-12-11 01:22:34 +0100

    Made changes to the about page

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -31,6 +31,10 @@ namespace SharpCodeWebsite.Controllers
         {
             return View();
         }
+        public IActionResult About()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
